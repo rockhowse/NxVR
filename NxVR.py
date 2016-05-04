de154f01f93c0dad66b74e4c562696f392a809d2
rockhowse <rockhowse@gmail.com>
2016-05-03 22:23:04 -0500

    added simple particles and updated three.js

--- NxVR.py
+++ NxVR.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def show_vr():
+def hello_world():
     return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0'
-    )
+    app.run()
